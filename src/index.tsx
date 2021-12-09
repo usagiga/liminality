8d098bc40053ea55a5d71fe3042a0566e0747d53
usagiga <usagiga@usagiga.me>
2021-12-09 23:45:03 +0900

    chore: add React Router tips

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,10 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+// If you want to route multiple pages,
+// use React Router.
+// https://create-react-app.dev/docs/adding-a-router/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
